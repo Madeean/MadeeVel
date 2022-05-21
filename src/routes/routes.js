3f2314322568c2345e3961297441c4ab07efe3e7
MadeReihan <reihanmade@gmail.com>
2022-05-21 17:36:41 +0700

    crud places,sliders,users. landing

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,6 +10,21 @@ import CategoriesIndex from '../pages/admin/categories/Index'
 import CategoryCreate from '../pages/admin/categories/Create'
 import CategoryEdit from '../pages/admin/categories/Edit'
 
+import PlacesIndex from '../pages/admin/places/Index'
+import PlaceCreate from '../pages/admin/places/Create'
+import PlaceEdit from '../pages/admin/places/Edit'
+
+import SlidersIndex from '../pages/admin/sliders/Index'
+import SliderCreate from '../pages/admin/sliders/Create'
+
+import UsersIndex from '../pages/admin/users/Index'
+import UserCreate from '../pages/admin/users/Create'
+import UserEdit from '../pages/admin/users/Edit'
+
+// =====================================================================================================
+
+import Home from '../pages/web/home/Index'
+
 function Routes(){
     return (
         <Switch>
@@ -33,6 +48,41 @@ function Routes(){
            <PrivateRoute exact path="/admin/categories/edit/:id">
                 <CategoryEdit /> 
             </PrivateRoute>
+
+
+           <PrivateRoute exact path="/admin/places">
+                <PlacesIndex /> 
+            </PrivateRoute>
+           <PrivateRoute exact path="/admin/places/create">
+                <PlaceCreate /> 
+            </PrivateRoute>
+           <PrivateRoute exact path="/admin/places/edit/:id">
+                <PlaceEdit /> 
+            </PrivateRoute>
+            
+           <PrivateRoute exact path="/admin/sliders">
+                <SlidersIndex /> 
+            </PrivateRoute>
+           <PrivateRoute exact path="/admin/sliders/create">
+                <SliderCreate /> 
+            </PrivateRoute>
+
+           <PrivateRoute exact path="/admin/users">
+                <UsersIndex /> 
+            </PrivateRoute>
+           <PrivateRoute exact path="/admin/users/create">
+                <UserCreate /> 
+            </PrivateRoute>
+           <PrivateRoute exact path="/admin/users/edit/:id">
+                <UserEdit /> 
+            </PrivateRoute>
+
+
+
+            <Route exact path="/">
+                <Home /> 
+            </Route>
+
             
         </Switch>
     )
